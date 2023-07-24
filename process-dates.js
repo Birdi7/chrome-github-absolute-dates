77440858904461982b6f3ceec5c0bb6ee70c1397
Egor <egor.osokin7@gmail.com>
2023-07-24 16:20:06 +0300

    fix

--- process-dates.js
+++ process-dates.js
@@ -4,12 +4,13 @@ for (var i in elements) {
     var el = elements[i];
 
     if (!el.attributes || !el.attributes.datetime) {
+
         continue;
     }
 
     // For some reason, this triggers right before the new page is loaded. If
     // we've already modified this relative-time, don't re-modify.
-    if (-1 !== el.innerHTML.indexOf(' (')) {
+    if (-1 !== el.innerHTML.indexOf(' (') || -1 !== el.outerHTML.indexOf(' (')) {
         continue;
     }
 
@@ -17,7 +18,7 @@ for (var i in elements) {
         el.attributes.datetime.value
     ));
 
-    var s = " (" + date.toLocaleDateString() + " " + date.toLocaleTimeString() + ")";
-    el.outerHTML += s;
-    el.innerHTML += s;
+    var s = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+    el.outerHTML = s;
+    el.innerHTML = s;
 }
